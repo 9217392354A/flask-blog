25bb76be1033d271af487f9727187b4c20d0e510
Chris <cfuller.ages@gmail.com>
2016-05-13 18:05:09 +0000

    bootstrap and templates

--- blog/views.py
+++ blog/views.py
@@ -8,18 +8,3 @@ def index():
     return 'Hello World!'
     
 
-"""
-from flask import render_template, redirect
-from author.form import RegisterForm
-
-
-@app.route('/login')
-def login():
-    return "Hello, User!"
-    
-    
-@app.route('/register', methods=('GET', 'POST'))
-def register():
-    form = RegisterForm()
-    return render_template('author/register.html', form=form)
-"""
